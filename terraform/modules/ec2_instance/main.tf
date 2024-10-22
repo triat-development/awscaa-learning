ab097a6f77c61f2cdbd2152aa57d112b3efdc07a
Ulric <karl.renaut@gmail.com>
2024-10-22 12:38:33 -0400

    Adding in GPT Created Terraform for a Bastion Server.  Will test later.

--- terraform/modules/ec2_instance/main.tf
+++ terraform/modules/ec2_instance/main.tf
@@ -1,10 +1,30 @@
 
+variable "ami_id" {
+  description = "AMI ID for launching the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance type for EC2"
+  type        = string
+}
+
+variable "security_group_id" {
+  description = "Security group ID to associate with EC2 instance"
+  type        = string
+}
+
+variable "user_data_script" {
+  description = "User data script to configure the EC2 instance"
+  type        = string
+}
+
 resource "aws_instance" "my_ec2_instance" {
-  ami                         = var.ami_id
-  instance_type               = var.instance_type
-  security_group_ids           = [var.security_group_id]
-  iam_instance_profile         = aws_iam_instance_profile.my_instance_profile.name
-  user_data                   = var.user_data_script
+  ami               = var.ami_id
+  instance_type     = var.instance_type
+  security_group_ids = [var.security_group_id]
+  iam_instance_profile = aws_iam_instance_profile.my_instance_profile.name
+  user_data         = var.user_data_script
 
   tags = {
     Name = "MyEC2Instance"
